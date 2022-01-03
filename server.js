95339dbeaf0465ccc10c9e4a7a1a47d6045164d3
Gokul's Laptop <gkowligi@gmail.com>
2022-01-03 15:02:05 -0800

    added HTML routes

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+const htmlRoutes = require("./routes/htmlRoutes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Use html routes
+app.use("/", htmlRoutes);
